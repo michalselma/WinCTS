0e4c7e8cf510c9bfcc9629af162ada462c4844ef
Michal Selma <michal@selma.cc>
2022-09-24 22:22:52 +0200

    update 22.9.24

---
+++ AppxPackages.ps1
@@ -0,0 +1,66 @@
+########################################
+# Package: Windows Appx Packages Modification Script
+# Type: CMD (Command Line) / PowerShell
+# Platform: Windows 10 / Windows 11
+# Source Code: https://github.com/michalselma/WinCTS
+########################################
+
+# Define config file location and name
+$appcfgfile = "$PSScriptRoot\config\appx-w10.conf"
+
+# Import script modules (functions)
+Import-Module -Name "$PSScriptRoot\AppxPackages.psm1" -ErrorAction Stop
+
+Write-Host "Generating currently installed appx apps list: \log\AppxSnapshot-*-before.list"
+$datetime = Get-Date -Format FileDateTime
+$appslistfile = "$PSScriptRoot\log\AppxSnapshot-$datetime-before.list"
+Get-AppxPackage -AllUsers | Format-Table -Property Name | Out-File -FilePath $appslistfile
+
+Write-Host "Starting Applications installation/uninstallation module."
+
+$removeapps, $addapps, $skipapps = ProcessAppxConfig($appcfgfile)
+Write-Host "Config file:" ($removeapps.length) "items for uninstallation."
+Write-Host "Config file:" ($addapps.length) "items for installation/reinstallation."
+Write-Host "Config file:" ($skipapps.length) "items to be skipped."
+
+if (($removeapps.length) -eq 0){
+	Write-Host "Nothing to be uninstalled."
+}
+else {
+	Write-Host "Please choose uninstalltion/removal option:"
+	Write-Host "1 - Current user uninstallation"
+	Write-Host "2 - All existing users uninstallation"
+	Write-Host "3 - Permanent Removal for all existing and future users"
+	$removaloption = Read-Host "Input '1', '2' or '3' to continue or ENTER to cancel)"
+	if ((('1') -contains $removaloption)) {
+		UninstallAppx_currentuser($removeapps)
+	}
+	elseif ((('2') -contains $removaloption)) {
+		UninstallAppx_currentuser($removeapps)
+		UninstallAppx_allusers($removeapps)
+	}
+	elseif ((('3') -contains $removaloption)) {
+		UninstallAppx_currentuser($removeapps)
+		UninstallAppx_allusers($removeapps)
+		RemoveAppx_SysProvisioned($removeapps)
+	}
+	else {
+		Write-Host "Uninstall/Removal cancelled..."
+	}
+}
+
+if (($addapps.length) -eq 0){
+	Write-Host "Nothing awaiting installation."
+}
+else {
+	InstallAppx_currentuser($addapps)
+}
+
+Write-Host "Finished Applications installation/uninstallation module."
+
+Write-Host "Generating appx apps that still exist: \log\AppxSnapshot-*-after.list"
+$datetime = Get-Date -Format FileDateTime
+$appslistfile = "$PSScriptRoot\log\AppxSnapshot-$datetime-after.list"
+Get-AppxPackage -allusers | Format-Table -Property Name | Out-File -FilePath $appslistfile
+
+Write-Host "Script finished."
--- README.md
+++ README.md
@@ -5,10 +5,14 @@ Windows operating system tweaks is package of command line and powershell script
 Functionality:
 - Power Options - modify behavior of power buttons and power saving options. There is separate configuration for Desktop and Laptop computer type.
 - Computer Name Change - modify windows machine/computer name.
+- AppxPackages - uninstall, permanently remove or reinstall Microsoft appx applications/packages for current user, all users or future users.
 
 Usage:
 - Review and/or edit content of the config files in config subfolder
 - Run particular cmd scripts with admin privileges
+
+Known Errors:
+- KE2209001 - AppxPackage uninstall for all users throws error at the end. After removal from last local user package will move automatically to staged status/user ({S-1-5-18 [S-1-5-18]: Staged}), which is not removabe by "Remove-AppxPackage" command. Regardless error package is being removed for all local users."
 
 AUTHOR:
 Michal Selma <michal@selma.cc>
